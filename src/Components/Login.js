94695c1fc41f93aa5369eaefe3f320bfdb1ad022
AKASH PS <akash.pradeep@oneteamus.com>
2024-03-09 10:32:42 +0530

    prettier extension enabled

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -20,14 +20,31 @@ const Login = () => {
       <form className="absolute p-12 bg-black w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-85">
         <h1 className="font-bold text-3xl pb-4">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         {!isSignInForm && (
-          <input type="text" placeholder="Full Name" className="p-2 my-2 w-full rounded-md bg-gray-700" />
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-2 my-2 w-full rounded-md bg-gray-700"
+          />
         )}
-        <input type="text" placeholder="Email or phone number" className="p-2 my-2 w-full rounded-md bg-gray-700" />
-        <input type="password" placeholder="Password" className="p-2 my-2 w-full rounded-md bg-gray-700" />
-        <button className="p-2 my-4 bg-red-600 w-full rounded-lg">{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="p-2 my-2 w-full rounded-md bg-gray-700"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full rounded-md bg-gray-700"
+        />
+        <button className="p-2 my-4 bg-red-600 w-full rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
         <p>
           {isSignInForm ? "New to Netflix?" : "Already a user?"}
-          <span onClick={toggleSignIn} className="text-decoration-line: underline cursor-pointer mx-2">
+          <span
+            onClick={toggleSignIn}
+            className="text-decoration-line: underline cursor-pointer mx-2"
+          >
             {isSignInForm ? "Sign Up" : "Sign In"}
           </span>{" "}
           now
